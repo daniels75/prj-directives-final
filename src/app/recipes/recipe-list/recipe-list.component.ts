2bb6faeaa0a391affa1803af25fbb2ee30ceefd7
Daniels <daniel.sadowski75@gmail.com>
2019-07-11 23:53:12 +0200

    Step rid of event emitters from components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,6 @@ import {RecipeService} from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [];
 
   constructor(private recipeService: RecipeService) { }
@@ -18,8 +17,6 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipies();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-   this.recipeWasSelected.emit(recipe);
-  }
+
 
 }
