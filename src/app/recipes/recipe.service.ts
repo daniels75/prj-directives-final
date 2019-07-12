85d001723232d9f8bfe7c8fe204174dda4605a15
Daniels <daniel.sadowski75@gmail.com>
2019-07-13 00:17:08 +0200

    improved recipe details - ingridents can be added to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,39 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>()
 
+  constructor(private shoppingServe: ShoppingListService) {
+
+  }
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('Hamburger',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Tomato', 3)
+      ]
+    ),
+    new Recipe('Chicken butter',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Chicken', 1),
+        new Ingredient('Garam masala', 20)
+      ])
   ];
 
   getRecipies() {
     return this.recipes.slice();
+  }
+
+  addIngredientToTheShoppingList(ingredients: Ingredient[]){
+    this.shoppingServe.addIngredients(ingredients);
   }
 }
